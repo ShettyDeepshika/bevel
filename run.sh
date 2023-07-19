b13a3b6ac1fdff0d6ce04864db21da8a03c409f2
ShettyDeepshika <mindtree09deepshika@gmail.com>
2023-07-19 11:24:19 +0000

    [ci skip] Deleting gitops release files
    Signed-off-by: ShettyDeepshika <mindtree09deepshika@gmail.com>

--- run.sh
+++ run.sh
@@ -37,5 +37,5 @@ sleep 10
 # ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/$configFile.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
 #  sleep 30 
 # echo "Running the playbook..."
-exec ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/$configFile.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset == 'false'"
+# exec ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/$configFile.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset == 'false'"
 
