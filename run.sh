18d7160b116ec35f94635c2254b8906cc9b880fc
root <root@ip-172-31-26-251.ec2.internal>
2023-07-19 07:30:15 +0000

    bevel

--- run.sh
+++ run.sh
@@ -7,6 +7,19 @@
 
 set -e
 
+#Creating network configuration file
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+
+ansible-playbook -vv /home/bevel/platforms/shared/configuration/config.yaml -e 'ansible_python_interpreter=/usr/bin/python3' --extra-vars "ordererOrg1=$ordererOrg1 peerOrg1=$peerOrg1 peerOrg2=$peerOrg2 peerOrg3=$peerOrg3 configFile=$configFile"
+
 echo "Starting build process..."
 
 echo "Adding env variables..."
@@ -18,6 +31,11 @@ KUBECONFIG=/home/bevel/build/config
 # echo "Validatin network yaml"
 # ajv validate -s /home/bevel/platforms/network-schema.json -d /home/bevel/build/network.yaml 
 
-echo "Running the playbook..."
-exec ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+sleep 10
+#Reset the network
+./reset.sh configFile=$configFile
+# ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/$configFile.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
+
+# echo "Running the playbook..."
+exec ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/$configFile.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset == 'false'"
 
